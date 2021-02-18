eb368a698f9c28301b8902a62f5577f3928a31d2
Emma Nopola <emma.nopola1@gmail.com>
2021-02-18 17:33:21 +0000

    cocktail

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,35 @@
+class CocktailsController < ApplicationController
+before_action :find_cocktail, only: [:show]
+
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.create(strong_params)
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render(:new)
+    end
+  end
+
+  private
+
+  def strong_params
+    params.require(:cocktail).permit(:name)
+  end
+
+  def find_cocktail
+    @cocktail = Cocktail.find(params[:id])
+  end
+end
+
+
---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,31 @@
+class DosesController < ApplicationController
+
+  def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
+
+  end
+
+  def create
+    @dose = Dose.new(strong_params)
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to cocktail_path(@cocktail.id)
+    else
+      render(:new) # render the new.html.erb
+    end
+
+  end
+
+  def destroy
+    @dose.ingredient_id.destroy
+    redirect_to cocktail_path
+  end
+
+   private
+
+  def strong_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
+end
